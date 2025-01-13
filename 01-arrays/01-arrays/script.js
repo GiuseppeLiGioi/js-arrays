ad4e37ef4b50131c4d099a0d26d1eb8613be9a59
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-13 12:30:48 +0100

    ADD: 7 tasks

--- 01-arrays/01-arrays/script.js
+++ 01-arrays/01-arrays/script.js
@@ -17,20 +17,45 @@ for(let i=0; i < teachers.length; i++){
 }
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+const fifth = "Patrick";
+teachers[4] = fifth;
+for(let i=0; i < teachers.length; i++){
+  console.log(teachers[i]); // il quinto nome sarà Patrick.
+}
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher); //ultimo insegnate: Luca
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher); //primo insegnate: Nathan
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
+for(let i=0; i < teachers.length; i++){
+  console.log(teachers[i]); // ultimo nome: Vanessa.
+}
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+for(let i=0; i < teachers.length; i++){
+  console.log(teachers[i]); // primo nome: Sarah.
+}
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = null;
+for(let i=0; i < teachers.length; i++){
+  if(teachers[i] === "Lewis"){
+    const lewisIndex = i;
+    console.log(lewisIndex); //indice di lewis nell'array teachers: 5.
+
+  }
+
+}
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
