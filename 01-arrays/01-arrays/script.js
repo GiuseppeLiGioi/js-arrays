22074bc6ad4e7fb6b2ec8013f2d2125b3b6745f6
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-13 12:14:33 +0100

    ADD: task1

--- 01-arrays/01-arrays/script.js
+++ 01-arrays/01-arrays/script.js
@@ -10,7 +10,11 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = "Giuseppe";
+teachers[3]=fourthTeacher;
+for(let i=0; i < teachers.length; i++){
+  console.log(teachers[i]); // il quarto nome sarà Giuseppe.
+}
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
