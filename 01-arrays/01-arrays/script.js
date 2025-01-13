7540ea789b373bf44c52c1ca4fca19810bf558e7
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-13 12:37:51 +0100

    ADD: task n8, fine esercizio (non bonus)

--- 01-arrays/01-arrays/script.js
+++ 01-arrays/01-arrays/script.js
@@ -59,4 +59,10 @@ for(let i=0; i < teachers.length; i++){
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+if(isTeachersEmpty > 0){
+  console.log("l'array è pieno")
+}
+  else{
+    console.log("l'array è vuoto")
+  }
